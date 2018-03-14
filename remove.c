e204b40d1062e145d4a27c74128bad5178547d04
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-03-14 15:58:29 +0530

    remove.c

---
+++ remove.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) 
+{
+	char str[200];
+	int i;
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==' ')
+		{
+			str[i]='+';
+		}
+	}
+	for(i=0;str[i]!='\0';i++)
+	{
+	if(str[i]!='+')
+	{
+	printf("%c",str[i]);
+	}
+	}
+	return 0;
+}
