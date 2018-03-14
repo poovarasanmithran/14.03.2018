a59cc8a51ea81fa6084c15a7382c1d672b763fb9
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-03-14 16:24:16 +0530

    range.c

---
+++ range.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+	int n;
+  int,m,i,j,k,count=0;
+	scanf("%d %d",&n,&m);
+	for(i=n;i<=m;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			k=j*j;
+			if(k==i)
+			{
+				count=count+1;
+				break;
+			}
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
